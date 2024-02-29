ebf921daa29a54a60c560565f7c40c9192e8e4ac
LORENZO LEAL MARANHO <lorenzo.lmaranho@senacsp.edu.br>
2024-02-28 22:17:28 -0300

    primeiro

---
+++ ConsoleApp1/ConsoleApp1/Control.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Control
+    {
+        public Control()
+        {
+            Console.WriteLine("\n\nEscolha uma das opções a baixo:  \n" +
+                "0. sair\n" +
+                "1. exercicio 01\n " +
+                "2. exercicio 02\n " +
+                "3. exercicio 03\n " +
+                "4. exercicio 04\n");
+        
+        
+        
+        
+        
+        
+        }//fim do menu 
+
+
+
+
+
+    }//fim da classe 
+}//fim do projeto 
---
+++ ConsoleApp1/ConsoleApp1/Model.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Model
+    {
+        //construtor
+
+        public Model(
+        {
+
+
+
+
+        }//fim do metodo construtor
+
+
+
+     // exercicio faça progrsama que imprima os numerods de 1 a 10 
+     public int ExercicioUm()
+        {
+            string resultado = "";
+
+
+            for(int i=0; < 10; i++)
+            {
+                resultado += "\n" +;
+            }
+            return resultado;
+
+
+        }//fim do metodo
+
+
+    }//fim da classe
+}// fim do projeto 
